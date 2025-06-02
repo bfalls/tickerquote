a03c166998341a2e8cfe3437fb663a0bb38f1444
bfalls <bfalls@outlook.com>
2025-06-01 18:56:43 -0600

    add frontend deploy

--- stock-strategy-app/vite.config.ts
+++ stock-strategy-app/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // relative paths for S3/CloudFront
   plugins: [react()],
-})
+});

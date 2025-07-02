d733b459517b723a2f1f167e69ec2a7b9d81bf65
bfalls <bfalls@outlook.com>
2025-07-01 18:35:27 -0600

    Fix dynu update and deployment

--- ec2/stream_price_data/update_dynu_ip.sh
+++ ec2/stream_price_data/update_dynu_ip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "[`date`] Starting Dynu IP update..."
+
 # Load Dynu API key securely from SSM
 API_KEY=$(aws ssm get-parameter \
   --name "DYNU_API_KEY" \
@@ -13,6 +15,8 @@ DOMAIN="stock-strategy.freedns.org"
 # Get public IP using IMDSv2
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
 CURRENT_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+echo "[`date`] Current public IP: $CURRENT_IP"
 
 # Send IP update to Dynu
-curl -s -X POST "https://api.dynu.com/nic/update?hostname=$DOMAIN&myip=$CURRENT_IP" -H "Auth-Token: $API_KEY" > ~/dynu_last_update.log
+RESPONSE=$(curl -s -X POST "https://api.dynu.com/nic/update?hostname=$DOMAIN&myip=$CURRENT_IP" -H "Auth-Token: $API_KEY")
+echo "[`date`] Dynu response: $RESPONSE" 
